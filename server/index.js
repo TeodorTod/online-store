0a48c627e28185fa2dfb99c90ba8812d983b9bbe
Artos Web <teodor.at.tod@gmail.com>
2022-10-18 15:24:09 +0300

    connect db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,26 @@
+const mongoose = require('mongoose');
 const express = require('express');
+// const cors = require('cors');
 
-const app = express();
+async function start() {
+    try {
+        const db = await mongoose.connect('mongodb://localhost:27017/furniture-rest', {
 
-app.get('/', (req, res) => {
-    res.json({message: "it works"})
-})
+        });
+        console.log('DB successfully connected');
+    } catch (err) { 
+        console.error(err);
+        return process.exit(1);
+    }
+    const app = express();
+    // app.use(cors());
 
-app.listen(3030, () => console.log('Server run on port:3030'))
+    // app.get('/data/catalog', (req, res) => {
+    //     res.json([])
+    // })
+
+    app.listen(3030, () => console.log('Server run on port:3030'))
+
+}
+
+start();
